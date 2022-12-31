25d5fa9061307fb947d473637d3ba4ed8cef5fca
warlord200 <95290000+warlord200@users.noreply.github.com>
2022-12-31 18:09:34 +0000

    Added generateBoard and displayController

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-const gameBoard = (() => {
-  let board = ["", "", "", "", "", "", "", "", ""];
+const game = (() => {
+  let board = [];
+  // generate the board
+  const generateBoard = () => {
+    for (let i = 1; i < 10; i++) {
+      const cell = {
+        id: i,
+        value: null,
+      };
+      board.push(cell);
+    }
+  };
+  return { board, generateBoard };
 })();
 
-const person = (name, symbol) => {
-  const getName = () => name;
-  const getSymbol = () => symbol;
-  return { getName, getSymbol };
-};
+const displayController = (() => {
+  const render = () => {
+    // render the board
+    const gameBoard = document.querySelector(".game-board");
+    gameBoard.innerHTML = "Hey";
+
+    // for (let i = 1; i < 10; i++) {
+    //   const cell = document.createElement("div");
+    //   cell.classList.add("cell");
+    //   cell.setAttribute("id", gameBoard.board[i].id);
+    //   gameBoard.appendChild(cell);
+    // }
+  };
+  return { render };
+})();
+
+game.generateBoard();
+displayController.render();
